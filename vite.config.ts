11b196877cd1c70999bd32f8224779807915508c
Younghoon <yhkang2003@gmail.com>
2024-11-30 17:51:51 +0900

    feat: Minimal entry point configuration with app icon links

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,31 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      injectRegister: "auto",
       registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js, css, html, ico, png, svg}"],
+      },
       devOptions: {
         enabled: true,
+      },
+      manifest: {
+        name: "Nangbomi",
+        short_name: "Nangbomi",
+        description: "Manage your refri with this app!",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./public/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./public/icon-512.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
