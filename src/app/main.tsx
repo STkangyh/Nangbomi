559cc4f03b4f4a6e47c947f4f9415e3699ad16a0
Younghoon <yhkang2003@gmail.com>
2024-12-01 01:45:00 +0900

    feat: service worker register basic setting

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppProvider from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onoffline() {},
+});
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
