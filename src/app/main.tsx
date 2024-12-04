662cff45de17076cd0da17892906fd05cd6fa3b9
Younghoon <yhkang2003@gmail.com>
2024-12-04 17:34:58 +0900

    feat: Initial Route re-settings

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import AppProvider from "./App.tsx";
+import AppProvider from "./App";
 
 createRoot(document.getElementById("root")!).render(<AppProvider />);
--- src/features/home/ui/expiredFoodList.tsx
+++ src/features/home/ui/expiredFoodList.tsx
@@ -1,22 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function ExpiredFoodList() {
   const [expiredFoodList, setExpiredFoodList] = useState([]);
   return (
     <div>
-      <div>
-        <div>소비 기한이 지난 식품</div>
-        <Link to="refri-list/:refriId">모두 보기</Link>
-      </div>
-      <div>
-        {expiredFoodList.map((food) => {
-          return (
-            <div>
-              <div>{food.name}</div>
-              <div>{food.expiredDate}</div>
-            </div>
-          );
-      </div>
+      <div>소비기한이 지난 상품</div>
+      <Link to="/expiredfoodlist">더보기</Link>
     </div>
   );
 }
