9477b94e6987d8ada7f3f02aa90b7fecf54fb503
Younghoon <yhkang2003@gmail.com>
2024-12-08 20:40:28 +0900

    feat: enhance BottomBar component with navigation functionality and icons

--- src/widgets/bottomBar/ui/bottomBar.tsx
+++ src/widgets/bottomBar/ui/bottomBar.tsx
@@ -1,15 +1,36 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function bottomBar() {
+import { Box, BottomNavigation, BottomNavigationAction } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import KitchenIcon from "@mui/icons-material/Kitchen";
+import PersonIcon from "@mui/icons-material/Person";
+
+export default function BottomBar() {
+  const [value, setValue] = useState(0);
+  const navigate = useNavigate();
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+    switch (newValue) {
+      case 0:
+        navigate("/");
+        break;
+      case 1:
+        navigate("/refri-list");
+        break;
+      case 2:
+        navigate("/mypage");
+        break;
+    }
+  };
+
   return (
-    <Box>
-      <BottomNavigation showLabels>
-        <BottomNavigationAction label="Home" />
-        <BottomNavigationAction label="Refri" />
-        <BottomNavigationAction label="My page" />
+    <Box sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
+      <BottomNavigation value={value} onChange={handleChange} showLabels>
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Refri" icon={<KitchenIcon />} />
+        <BottomNavigationAction label="My page" icon={<PersonIcon />} />
       </BottomNavigation>
     </Box>
   );
