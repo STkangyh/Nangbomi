04ce8b6638cedee0dbf7e1535fca286c12511283
Younghoon <yhkang2003@gmail.com>
2024-12-08 15:22:22 +0900

    feat: implement signup form with MUI components and fetcher

--- src/pages/signup/ui/signup.tsx
+++ src/pages/signup/ui/signup.tsx
@@ -1,3 +1,45 @@
+import { useFetcher } from "react-router-dom";
+import Title from "../../../features/sign/ui/Title";
+
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  Button,
+} from "@mui/material";
+
 export default function Signup() {
-  return <>Signup</>;
+  const fetcher = useFetcher();
+  return (
+    <Box className="p-4">
+      <Title />
+      <fetcher.Form method="post" className="flex flex-col space-y-4">
+        <FormControl fullWidth>
+          <InputLabel htmlFor="name-input">이름</InputLabel>
+          <OutlinedInput id="name-input" defaultValue="홍길동" label="Name" />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="email-input">이메일</InputLabel>
+          <OutlinedInput
+            id="email-input"
+            defaultValue="Your-email@gmail.com"
+            label="Email"
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{
+            backgroundColor: "black",
+            "&:hover": {
+              backgroundColor: "#333",
+            },
+          }}
+        >
+          시작하기
+        </Button>
+      </fetcher.Form>
+    </Box>
+  );
 }
