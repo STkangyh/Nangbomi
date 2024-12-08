7bf8cc280a8977cc135339c1d3951a7376033058
Younghoon <yhkang2003@gmail.com>
2024-12-08 16:00:08 +0900

    feat: update title component to display dynamic text based on route

--- src/features/sign/ui/Title.tsx
+++ src/features/sign/ui/Title.tsx
@@ -2,7 +2,11 @@ export default function Title() {
   return (
     <div className="mb-10">
       <div className="text-sm mb-2">냉보미를 시작해요</div>
-      <div className="text-2xl font-bold">계정 생성하기</div>
+      <div className="text-2xl font-bold">
+        {window.location.pathname.includes("signup")
+          ? "계정 생성하기"
+          : "로그인할 계정 선택하기"}
+      </div>
     </div>
   );
 }
